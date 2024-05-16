c19b15c41f38283105d4ab43061e6a4aeadec11e
Jony <jonybhattacharjee886@gmail.com>
2024-05-16 21:20:48 +0600

    Custom error for form

---
+++ src/components/custom-error.tsx
@@ -0,0 +1,14 @@
+import { TriangleAlert } from "lucide-react";
+
+interface ErrorProps {
+  message?: string;
+}
+export const CustomError = ({ message }: ErrorProps) => {
+  if (!message) return null;
+  return (
+    <div className="flex items-center gap-x-2 rounded-md bg-destructive/15 p-3 text-sm text-destructive">
+      <TriangleAlert className="h-4 w-4" />
+      <p>{message}</p>
+    </div>
+  );
+};
