b4dd921b98d5d6a92a5ecb93957f95f9848ab029
Jony <jonybhattacharjee886@gmail.com>
2024-05-22 00:19:48 +0600

    Fix responsive issue

--- src/app/(landing page)/_components/featured-list.tsx
+++ src/app/(landing page)/_components/featured-list.tsx
@@ -10,7 +10,7 @@ export const FeaturedList = () => {
   return (
     <div className="my-16 bg-muted">
       <div className="container mx-auto py-24">
-        <div className="flex flex-col items-start justify-between gap-8 md:flex-row md:items-center md:gap-0">
+        <div className="flex flex-col  justify-between gap-8 md:flex-row items-center md:gap-0">
           <div className="flex flex-col items-center gap-2 md:items-start">
             <h2 className="text-center text-3xl font-semibold tracking-tight md:text-left">
               Discover Our Featured Listings
@@ -21,23 +21,23 @@ export const FeaturedList = () => {
           </div>
           <Link
             href="#"
-            className="flex items-center gap-2.5 transition-colors duration-100 ease-in-out hover:text-primary/50"
+            className="flex items-center  gap-2.5 transition-colors duration-100 ease-in-out hover:text-primary/50"
           >
             See all Properties
             <ArrowUpRight className="h-4 w-4 transition-colors duration-100 ease-in-out hover:text-primary/50" />
           </Link>
         </div>
-        <div className="my-16 flex flex-col items-start justify-between gap-8 md:flex-row md:items-center">
+        <div className="my-16 flex flex-col items-start justify-between gap-8 md:flex-row md:items-center ">
           {/** Items */}
           {featured.map((data) => (
-            <div key={data.id}>
+            <div key={data.id} className="w-full">
               <div className="relative rounded-tl-xl rounded-tr-xl">
                 <Image
                   src={data.image}
                   alt={data.name}
                   width={300}
                   height={300}
-                  className=" rounded-tl-xl rounded-tr-xl object-cover"
+                  className=" w-full rounded-tl-xl rounded-tr-xl object-cover md:w-auto"
                 />
                 <div className="absolute left-0 top-4 ml-4 flex items-center gap-2.5 rounded bg-destructive px-4 py-2 text-white">
                   <Zap className="h-4 w-4 text-white" />
@@ -75,9 +75,9 @@ export const FeaturedList = () => {
                   </div>
                 </div>
                 <Separator />
-                <div className=" my-4 flex flex-col items-start justify-between md:flex-row md:items-center">
+                <div className=" my-4 flex items-center justify-between">
                   <span className="text-sm">For Rent</span>
-                  <div className="flex flex-col items-start gap-4 md:flex-row md:items-center">
+                  <div className="flex items-center gap-4">
                     <ExternalLink className="h-4 w-4 text-muted-foreground" />
                     <LuCopyPlus className="h-4 w-4 text-muted-foreground" />
                     <Heart className="h-4 w-4 text-muted-foreground" />
--- src/app/(landing page)/_components/hero.tsx
+++ src/app/(landing page)/_components/hero.tsx
@@ -4,15 +4,17 @@ export const Hero = () => {
   return (
     <div className="hero-bg">
       <div className="flex h-dvh w-full items-center">
-        <div className="container mx-auto">
-          <div className="flex flex-col items-center gap-2 text-white">
+        <div className="container mx-auto mt-16">
+          <div className="flex flex-col items-center gap-8 text-white md:gap-2">
             <h6 className="uppercase">The Best way to</h6>
-            <h2 className="text-center text-5xl font-bold capitalize tracking-wide">
+            <h2 className="text-center text-5xl font-semibold capitalize tracking-wide">
               Find your dream home
             </h2>
-            <p>We’ve more than 745,000 apartments, place & plot.</p>
+            <p className="text-center">
+              We’ve more than 745,000 apartments, place & plot.
+            </p>
           </div>
-          <div className="my-4 px-36">
+          <div className="my-4 hidden px-36 md:flex">
             <SearchFilter />
           </div>
         </div>
