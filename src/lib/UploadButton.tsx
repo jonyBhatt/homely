6d75cedd8e3776b360bee94c8e13a1e77e6fcd52
Jony <jonybhattacharjee886@gmail.com>
2024-06-03 23:39:43 +0600

    Setting Cloudinary

---
+++ src/lib/UploadButton.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+"use client";
+import { Upload } from "lucide-react";
+import { CldUploadButton } from "next-cloudinary";
+interface UploadProps {
+  onChange: (url?: string) => void;
+  className?: string;
+}
+export default function UploadButton({ onChange, className }: UploadProps) {
+  return (
+    <CldUploadButton
+      uploadPreset="c2jrssik"
+      onSuccess={(results) => {
+        //@ts-ignore
+        // console.log(results?.info.url);
+        if (results.info) {
+          //@ts-ignore
+          onChange(results.info.url);
+        }
+      }}
+    >
+      <span className={`${className}  flex items-center gap-4`}>
+        <Upload className="h-4 w-4" />
+        Upload
+      </span>
+    </CldUploadButton>
+  );
+}
