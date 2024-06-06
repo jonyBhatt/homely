55067bbfd4bbd2d4f32f9fd633b07950b9c22b2d
Jony <jonybhattacharjee886@gmail.com>
2024-06-07 00:44:28 +0600

    Fetch property

--- src/app/(landlord)/l-dashboard/my-property/_components/PropertyCard.tsx
+++ src/app/(landlord)/l-dashboard/my-property/_components/PropertyCard.tsx
@@ -3,42 +3,46 @@ import { Separator } from "../../../../../components/ui/separator";
 import { Button } from "../../../../../components/ui/button";
 
 type PropertyCardProps = {
-  property: {
-    id: number;
-    title: string;
-    description: string;
-    image: string;
-    bedrooms: string;
-    bathrooms: string;
-    location: string;
-  };
+  title: string;
+  description: string;
+  image: string;
+  bedrooms: string;
+  bathrooms: string;
+  state: string;
+  address: string;
+  country: string;
 };
 
-export const PropertyCard = ({ property }: PropertyCardProps) => {
+export const PropertyCard = ({
+  title,
+  description,
+  image,
+  bedrooms,
+  bathrooms,
+  state,
+  address,
+  country,
+}: PropertyCardProps) => {
   return (
     <div className="mb-8 w-full px-4 sm:w-1/2 lg:w-1/3">
       <div className="overflow-hidden rounded-lg bg-white shadow-lg">
-        <img
-          className="h-64 w-full object-cover"
-          src={property.image}
-          alt={property.title}
-        />
+        <img className="h-64 w-full object-cover" src={image} alt={title} />
         <div className="p-4">
-          <h2 className="mb-2 text-lg font-semibold">{property.title}</h2>
+          <h2 className="mb-2 text-lg font-semibold">{title}</h2>
           <span className="mb-4 text-sm text-muted-foreground">
-            {property.location}
+            {address}, {state}, {country}
           </span>
           <div className="mb-4 flex items-center gap-4 text-sm text-gray-700">
             <div className="flex flex-col gap-1 sm:flex-row">
               <Bed className="h-5 w-5 text-muted-foreground" />
               <span className="text-balance text-muted-foreground">
-                {property.bedrooms} Bedrooms
+                {bedrooms} Bedrooms
               </span>
             </div>
             <div className="flex flex-col gap-1 sm:flex-row">
               <Bath className="h-5 w-5 text-muted-foreground" />
               <span className="text-balance text-muted-foreground">
-                {property.bedrooms} Bathrooms
+                {bathrooms} Bathrooms
               </span>
             </div>
           </div>
--- src/app/(landlord)/l-dashboard/my-property/page.tsx
+++ src/app/(landlord)/l-dashboard/my-property/page.tsx
@@ -1,13 +1,28 @@
 import { PropertyCard } from "~/app/(landlord)/l-dashboard/my-property/_components/PropertyCard";
-import { properties } from "~/utils/mock/property-data";
+// import { properties } from "~/utils/mock/property-data";
+import prisma from "~/server/db";
 
-function MyProperty() {
+async function MyProperty() {
+  const properties = await prisma.property.findMany();
+  if (!properties || properties.length === 0) {
+    return <p>No properties available</p>;
+  }
   return (
     <div className="container mx-auto px-4">
       <h1 className="my-8  text-3xl font-semibold">My Properties</h1>
       <div className="flex flex-wrap justify-center">
         {properties.map((property) => (
-          <PropertyCard key={property.id} property={property} />
+          <PropertyCard
+            key={property.id}
+            title={property.title}
+            description={property.description}
+            image={property.image ?? ""} // Assuming image can be null, so we use the empty string as a fallback
+            bedrooms={property.bedrooms}
+            bathrooms={property.bathrooms}
+            state={property.state ?? ""} // Assuming state can be null, so we use the empty string as a fallback
+            address={property.address}
+            country={property.country ?? ""}
+          />
         ))}
       </div>
     </div>
