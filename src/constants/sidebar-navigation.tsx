f654557f1ecf7b3107e70ef074f6a00861970566
jony <jonybhattcharjee886@gmail.com>
2024-10-19 01:00:07 +0600

    Added landlord sidebar contents

--- src/constants/sidebar-navigation.tsx
+++ src/constants/sidebar-navigation.tsx
@@ -8,7 +8,46 @@ import { TiMessages } from "react-icons/ti";
 import { RiHome3Line } from "react-icons/ri";
 import { CgProfile } from "react-icons/cg";
 import { TbBrandBooking } from "react-icons/tb";
+
 export const AgentSideBarContents = [
+  {
+    title: "Dashboard",
+    path: "/agent-dashboard",
+    icon: <MdOutlineDashboard />,
+  },
+  {
+    title: "Bookings",
+    path: "/agent-dashboard/message",
+    icon: <TbBrandBooking />,
+  },
+  {
+    title: "My Property",
+    path: "/agent-dashboard/my-property",
+    icon: <RiHome3Line />,
+  },
+  {
+    title: "Add Property",
+    path: "/agent-dashboard/add-property",
+    icon: <MdOutlineAddToPhotos />,
+  },
+  {
+    title: "My Favorites",
+    path: "/agent-dashboard/my-fav",
+    icon: <FaRegHeart />,
+  },
+  {
+    title: "Profile",
+    path: "/agent-dashboard/profile",
+    icon: <CgProfile />,
+  },
+  {
+    title: "Review",
+    path: "/agent-dashboard/review",
+    icon: <MdOutlineRateReview />,
+  },
+];
+
+export const LandlordSideBarContents = [
   {
     title: "Dashboard",
     path: "/agent-dashboard",
