9fa9e68a4499e656be256063452287723122f055
jony <jonybhattcharjee886@gmail.com>
2024-10-22 21:40:48 +0600

    Added agency schema

--- src/utils/validation/index.tsx
+++ src/utils/validation/index.tsx
@@ -53,3 +53,34 @@ export const scheduleSchema = z.object({
   email: z.string().email(),
   date: z.string(),
 });
+
+export const agencySchema = z.object({
+  agencyname: z
+    .string()
+    .min(2, {
+      message: "Agency name must be at least 2 characters.",
+    })
+    .max(50, { message: "Agency name should not exit 50 character" }),
+
+  address: z.string().min(1, { message: "Address required" }),
+  email: z
+    .string()
+    .email({ message: "Give a valid email!" })
+    .refine(
+      (email) => {
+        return email.endsWith("@company.com");
+      },
+      {
+        message: "Email must be from the domain @company.com",
+      },
+    ),
+
+  image: z.string().optional(),
+  phone: z
+    .string()
+    .regex(/^(\+8801(7|6)\d{8})$/, {
+      message:
+        "Phone number must start with +880 and contain 11 digits with 017 or 016 after +880",
+    })
+    .max(11),
+});
