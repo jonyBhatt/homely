5ca53df784583a4642871e5ea67209452510cde3
jony <jonybhattcharjee886@gmail.com>
2024-10-22 21:41:06 +0600

    Agency page

---
+++ src/app/(landlord)/landlord-dashboard/agency/page.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { CurrentUser } from "~/lib/current-user";
+import prisma from "~/server/db";
+async function Agency() {
+  const { user } = await CurrentUser();
+  const existAgency = await prisma.agency.findFirst({
+    where: {
+      landlordId: user?.id,
+    },
+  });
+  return (
+    <div>
+      {existAgency ? (
+        <></>
+      ) : (
+        <div>
+          <h2 className="border-b border-b-[#405189] pb-2  text-3xl font-semibold">
+            Agency
+          </h2>
+          <div className="flex min-h-dvh w-full flex-col justify-center gap-8">
+            <h2 className="text-3xl font-semibold">
+              You don&apos;t owned any agency
+            </h2>
+            <Link href="/landlord-dashboard/agency/create">
+              <Button className="w-full text-lg font-medium" size="lg">
+                Create Agency
+              </Button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Agency;
