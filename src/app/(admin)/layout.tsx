42e61474e96592ae0a2c4a275cee586afeeef6b9
jony <jonybhattcharjee886@gmail.com>
2024-12-21 14:44:42 +0600

    Fix: Session passed as props

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,11 +1,18 @@
 import { ReactNode } from "react";
+import { auth } from "~/auth";
 import { AdminHeader } from "./_components/AdminHeader";
 
-export default function AdminLayout({ children }: { children: ReactNode }) {
+export default async function AdminLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await auth();
+  if (!session) return null;
   return (
     <div className=" mx-auto flex min-h-dvh flex-col ">
       {/** Header */}
-      <AdminHeader />
+      <AdminHeader session={session} />
       {/** Mobile First */}
       <div className="flex lg:hidden"></div>
     </div>
