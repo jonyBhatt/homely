036ebd13c4bcf182d3249c43d7ff9fac6dd32bf4
jony <jonybhattcharjee886@gmail.com>
2025-01-06 13:30:50 +0600

    Added get all application, Single Application, publish application

--- src/server/action/agent/index.ts
+++ src/server/action/agent/index.ts
@@ -3,6 +3,8 @@
 import { z } from "zod";
 import { CurrentUser } from "~/lib/current-user";
 import { agentFormSchema } from "~/utils/validation";
+import prisma from "~/server/db";
+import { revalidatePath } from "next/cache";
 
 export const agentForm = async (values: z.infer<typeof agentFormSchema>) => {
   const { user } = await CurrentUser();
@@ -15,7 +17,7 @@ export const agentForm = async (values: z.infer<typeof agentFormSchema>) => {
   }
 
   try {
-    await prisma?.agentApplication.create({
+    await prisma.agentApplication.create({
       data: {
         title: fieldValues.data.title,
         description: fieldValues.data.description,
@@ -36,5 +38,84 @@ export const agentForm = async (values: z.infer<typeof agentFormSchema>) => {
     return {
       error: error,
     };
+  }
+};
+
+// Get agent application
+
+export const getAgentApplications = async () => {
+  try {
+    const application = await prisma.agentApplication.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+    return {
+      message: "Successfully fetched",
+      application,
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+// Get single  agent application'
+export const getSingleAgentApplication = async (id: string) => {
+  try {
+    const application = await prisma.agentApplication.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+    return {
+      message: "Successfully fetched",
+      application,
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+// Update agent application
+export const publishAgentApplication = async (id: string) => {
+  try {
+    await prisma.agentApplication.update({
+      where: {
+        id,
+      },
+      data: {
+        published: true,
+        status:"APPROVED",
+        user: {
+          update: {
+            role: "Agent",
+          },
+        },
+      },
+    });
+    revalidatePath('/admin')
+    return {
+      message: "Successfully published",
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 };
