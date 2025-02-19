356954d7370660ebba412c57da381d4c12ccf1c6
Jonybhatt5669 <jitbjony5669@gmail.com>
2025-02-19 20:11:44 +0600

    Added landlord applications

--- src/server/action/user/index.ts
+++ src/server/action/user/index.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use server";
 import { auth } from "~/auth";
+import { CurrentUser } from "~/lib/current-user";
 import prisma from "~/server/db";
+import { LandlordApplicationData } from "~/types/lanlord-interface";
 
 export const getCurrentUser = async () => {
   const getUser = await auth();
@@ -65,6 +70,80 @@ export const getUsers = async () => {
     return {
       message: "Fetching users successfully",
       users,
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+/**
+ * Landlord application
+ */
+
+export const applyLandlord = async (values: LandlordApplicationData) => {
+  const user = await CurrentUser();
+  try {
+    await prisma.user.update({
+      where: {
+        id: user.user?.id,
+      },
+      data: {
+        LandlordApplication: {
+          create: {
+            ...values,
+          },
+        },
+      },
+    });
+    return {
+      message: "Application submitted successfully",
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+/**
+ * Get Landlord
+ */
+export const getLandlord = async (id: string) => {
+  try {
+    const landlord = await prisma.landlordApplication.findUnique({
+      where: {
+        id,
+      },
+    });
+    return {
+      message: "Fetching landlord successfully",
+      landlord,
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+/**
+ * Update role as  Landlord
+ */
+export const updateRoleAsLandlord = async (id: string) => {
+  try {
+    const user = await prisma.landlordApplication.update({
+      where: {
+        id,
+      },
+      data: {
+        user: {
+          update: {
+            role: "Landlord",
+          },
+        },
+      },
+    });
+    return {
+      message: "Role updated successfully",
+      user,
     };
   } catch (error) {
     console.log(error);
